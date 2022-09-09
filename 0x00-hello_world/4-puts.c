98c8af5319faa0e62152e342f71591f1d349bd82
Gamaliel Adun <adungamaliel@gmail.com>
2022-09-09 03:37:56 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/* This is a programme that uses puts to output a message to CLI */
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str1[52];
+
+	strcpy(str1, "Programming is like building a mulitlingual puzzle\n");
+	puts(str1);
+	return (0);
+}
