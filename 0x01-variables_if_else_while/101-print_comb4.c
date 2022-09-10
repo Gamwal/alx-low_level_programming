da990381b0c2761a349edb16387aafee3fd316b0
Gamaliel <adungamaliel@gmail.com>
2022-09-10 11:41:09 +0000

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j;
+	int k;
+
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			k = 0;
+			while (k <= 9)
+			{
+				if (j > i && k > j && i != j && j != k && i != k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+				
+					if (i + j + k != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+			k++;
+		j++;
+		}
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
