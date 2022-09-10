d44f2a3fdbc39a2555a61ccd26f34c3501680d52
Gamaliel <adungamaliel@gmail.com>
2022-09-10 05:24:42 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints 0-9 using putchar()
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		putchar(d +'0');
+		d++;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
