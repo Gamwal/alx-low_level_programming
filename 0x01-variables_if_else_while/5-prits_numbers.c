791a5a92e566eb1054dbd7498773c6a1d6979732
Gamaliel <adungamaliel@gmail.com>
2022-09-10 05:08:55 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-prits_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int dig;
+
+	dig = 0;
+
+	while (dig <= 9)
+	{
+		printf("%d", dig);
+			dig++;
+	}
+	printf("\n");
+
+
+
+
+
+
+
+}
