3b79b4983227b5fb6898208adf5b8301d4c20473
Gamaliel <adungamaliel@gmail.com>
2022-09-13 20:12:26 +0000

    Number 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,12 @@
 
 /**
  * print_last_digit - prints last digit
+ * @n: Number to print last digit from
  *
  * Return: Always 0 (success)
  */
 
-int prnt_last_digit(int n)
+int print_last_digit(int n)
 {
 	return (n % 10);
 	return (0);
