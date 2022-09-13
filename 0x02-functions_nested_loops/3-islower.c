0d2c3b493846bc49722c1d24641cd2b3ef8fc181
Gamaliel Adun <adungamaliel@gmail.com>
2022-09-13 08:56:26 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ *
+ * Description: Checks for lowercase character
+ *
+ * Return: 0 Always (Success).
+ */
+
+int _islower(int c);
+{
+
+
+}
