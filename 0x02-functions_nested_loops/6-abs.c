93aece75c589b6fe807a084461b868a3021f71d5
Gamaliel <adungamaliel@gmail.com>
2022-09-13 20:02:54 +0000

    Asolut e value finder

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute number
+ * @int: number to be checked
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int)
+{
+	if (int > 0)
+		_putchar(int);
+	else if (int < 0)
+		_putchar(0 - int);
+	else
+		_putchar(0);
+	return (0);
+
+}
