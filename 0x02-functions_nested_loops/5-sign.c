aa667cebe4075114a27b16ff7483a1915cc847fd
Gamaliel <adungamaliel@gmail.com>
2022-09-13 19:25:48 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign - check polarity
+ * @n: number to be checked
  *
  * Description: Checks polarity and prints out the sign
  *
@@ -11,12 +12,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
+	}
 	else if (n == 0)
+	{
 		_putchar('0');
 		return (0);
+	}
 	else if
+	{
 		_putchar('-');
 		return (-1);
+	}
 }
