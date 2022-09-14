050dd7d50a596da730e6244e3589a102387a0c41
Gamwal <52507933+Gamwal@users.noreply.github.com>
2022-09-14 14:43:44 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -40,8 +40,8 @@ void print_times_table(int n)
 				}
 				else if (product >= 100)
 				{
-					_putchar((prod / 100) + 0);
-					_putchar((prod / 10) % 10 + 0);
+					_putchar((product / 100) + 0);
+					_putchar((product / 10) % 10 + 0);
 				}
 			}
 			_putchar('\n');
