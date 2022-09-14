a7709680e74931ef949118cc1316e2aeacd4ccb6
Gamaliel <adungamaliel@gmail.com>
2022-09-14 19:26:00 +0000

    Nawa

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,9 +31,14 @@ void print_times_table(int n)
 					_putchar(((product % 100) / 10) + '0');
 					_putchar(((product % 100) % 10) + '0');
 				}
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (j < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				if (product < 10)
+					_putchar(' ');
 			}
 			_putchar('\n');
 		}
