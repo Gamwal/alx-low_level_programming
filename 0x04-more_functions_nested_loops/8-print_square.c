83f4aa6813c12b11af4f3d430f5ef6aa26567a75
Gamaliel Adun <adungamaliel@gmail.com>
2022-09-15 05:54:22 -0700

    number 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,23 @@
 #include "main.h"
 
 /**
+ * print_square - prints a square followed by a new line
+ * @size: the size of the reservoirs
  *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 1, i <= size; i++)
+	{
+		for (j = 1, j <= size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
