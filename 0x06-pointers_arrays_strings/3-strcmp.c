62adf585c0414a9a5e598f902efcc85e1e88cfdd
Gamaliel <adungamaliel@gmail.com>
2022-09-21 21:22:09 +0000

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: results similar to standard strcmp
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	if (i > j)
+		return (15);
+	else if (i == j)
+		return (0);
+	else
+		return (-15);
+}
