7b9a9c688e73c6f66930ee3149b3824333a5fe47
Gamaliel Adun <adungamaliel@gmail.com>
2022-09-21 07:37:41 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,15 +2,28 @@
 
 /**
  * *_strcat - function that concatenated two strings
- * @*dest - base string
- * @*src - string to be added
+ * @dest: base string
+ * @src: string to be added
  *
  * Return: a pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)
 {
+	int i = 0;
+	int j = 0;
 
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
 
-
+	return (dest);
 }
