60a23233ee0af88bc2f1bd380a0b242b612469fd
Gamaliel Adun <adungamaliel@gmail.com>
2022-09-21 06:51:05 -0700

    Initial Commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *_strcat - function that concatenated two strings
+ * @*dest - base string
+ * @*src - string to be added
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+
+
+}
