043bc1410c01b15ca2a34cb5efafb17e92e1635b
Gamaliel Adun <adungamaliel@gmail.com>
2022-09-22 01:10:23 -0700

    Update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
-
-	while (s1[i] != '\0')
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		i++;
+		s1++;
+		s2++;
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	return (i - j);
+	return (*s1 - *s2);
 }
