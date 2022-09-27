c3a6a3ed20699df512646f68fe5974783257055d
Gamaliel <adungamaliel@gmail.com>
2022-09-27 04:20:47 +0000

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,9 +11,9 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n && src[i] != '\0')
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 		i++;
