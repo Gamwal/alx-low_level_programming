230ca676c995436be3072073649914400800bf2d
Gamaliel <adungamaliel@gmail.com>
2022-09-28 08:52:04 +0000

    First Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s != '\0')
+	{
+		_purchar(s);
+		_puts_recursion(s++);
+	}
+	putchar('\n');
+}
