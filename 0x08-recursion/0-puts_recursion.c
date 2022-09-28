379d0a0030dcd9e08803572ba7de80ded96d73b1
Gamaliel <adungamaliel@gmail.com>
2022-09-28 08:59:22 +0000

    First Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,10 @@
 
 void _puts_recursion(char *s)
 {
-	if (s != '\0')
+	if (*s != '\0')
 	{
-		_purchar(s);
-		_puts_recursion(s++);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
