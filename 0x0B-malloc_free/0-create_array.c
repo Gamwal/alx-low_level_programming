e689cd4f35bcec462ffc72e20024c80c8c5f40ea
Gamaliel Adun <adungamaliel@gmail.com>
2022-10-04 01:33:23 -0700

    add 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,12 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
+
 	tab = malloc(sizeof(char) * size);
+	
+	if (tab == NULL)
+		return (NULL);
+	
 	for (i = 0; i < size; i++)
 	{
 		tab[i] = c;
