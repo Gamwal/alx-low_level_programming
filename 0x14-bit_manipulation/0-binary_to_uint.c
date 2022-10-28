33ef65bd794b03c163d27dca1c649f2ed69560f8
Gamaliel <adungamaliel@gmail.com>
2022-10-28 07:54:15 +0000

    added mandatory tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,28 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int count = 0;
-	int i;
+	unsigned int uns_int = 0;
+	int len, base_two;
 
-	for (i = 0; i < sizeof(b); i++)
+	if (!b)
+		return (0);
+
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[i] != '0' || b[i] != '1')
-			return (NULL);
-		else
-			count++;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			uns_int += base_two;
+		}
 	}
 
-	for (i = count; i >= 0; i--)
-	{
-		for (j = 0; sum += b[i] * 2
+	return (uns_int);
+}
