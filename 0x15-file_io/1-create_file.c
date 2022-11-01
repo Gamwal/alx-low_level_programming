f3a15d4ba03f82c19d0a1bf73904ecf8d1f73334
Gamaliel Adun <adungamaliel@gmail.com>
2022-11-01 07:26:27 -0700

    added 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_file - fuction that creates a file
+ * @filename: name of the file to be created
+ * @text_content: file content
+ *
+ * Return: 1 on success or -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fo, count;
+	ssize_t fw;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		count = sizeof(text_content);
+
+	fo = open(filename, O_CREAT | O_WRONLY, 00600);
+	
+	fw = write(fo, text_content, count);
+
+	if (fo == -1 || fw == -1)
+		return (-1);
+
+	close(fo);
+
+	return (1);
+}
