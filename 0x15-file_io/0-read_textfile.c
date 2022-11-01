cd547ecd4d8edce83484f57482c8fcf079357ee3
Gamaliel Adun <52507933+Gamwal@users.noreply.github.com>
2022-11-01 12:23:44 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,15 +2,15 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX
- * 		standard output
+ *		standard output
  * @filename: pointer to the file
  * @letters: numbers of letters to read and print
  *
  * Return: actual number of letters read and printed, or
- * 		0 if filename == NULL, or
- * 		if file cannot be opened or read, or
- * 		if write fails or does not write the expected
- * 		number of bytes.
+ *		0 if filename == NULL, or
+ *		if file cannot be opened or read, or
+ *		if write fails or does not write the expected
+ *		number of bytes.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
