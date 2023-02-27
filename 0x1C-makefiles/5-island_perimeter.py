7113b77fdc4bf08e8409dcdd10e416da1735cb2e
Gamaliel Adun <52507933+Gamwal@users.noreply.github.com>
2023-02-27 13:46:59 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def island_perimeter(grid):
-    shape_of_grid = grid.shape
+    nrows = len(grid)
+    ncols = len(grid[0])
     perimeter = 0
-    for i in range(shape_of_grid[0]):
-        for j in range(shape_of_grid[1]):
+    for i in range(nrows):
+        for j in range(ncols):
             p = 4
             if grid[i][j] == 1:
                 if i != 0:
@@ -13,10 +14,12 @@ def island_perimeter(grid):
                 if j != 0:
                     if grid[i][j - 1] == 1:
                         p -= 1
-                if (j + 1) != shape_of_grid[1]:
+                if (j + 1) != ncols:
                     if grid[i][j + 1] == 1:
                         p -= 1
-                if (i + 1) != shape_of_grid[0]:
+                if (i + 1) != nrows:
                     if grid[i + 1][j] == 1:
                         p -= 1
                 perimeter += p
+                
+    return(perimeter)
