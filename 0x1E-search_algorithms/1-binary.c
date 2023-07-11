2afa520e2a1580b6b35fd14b1f862c3b564e42a8
Gamaliel Adun <adungamaliel@gmail.com>
2023-07-11 11:34:30 +0000

    updated 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-#include "search_algos.h"
 
 /**
  * binary_search - function that searches for a value in a sorted
@@ -13,48 +12,32 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t i, left, right;
-    int m;
+	size_t i, left, right;
+	int m;
 
-    left = 0;
-    right = size - 1;
+	left = 0;
+	right = size - 1;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
-    else
-    {
-        while (left <= right)
-        {
-            printf("Searching in array: ");
-            for (i = left; i <= right; i++)
-            {
-                printf("%d", array[i]);
-                if (i < right)
-                {
-                    printf(", ");
-                }
-                else
-                {
-                    printf("\n");
-                }
-            }
-            m = floor((left + right) / 2);
-            if (array[m] < value)
-            {
-                left = m + 1;
-            }
-            else if (array[m] > value)
-            {
-                right = m - 1;
-            }
-            else
-            {
-                return (m);
-            }
-            
-        }
-    }
-    return (-1);
+	if (array == NULL)
+		return (-1);
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+			else
+				printf("\n");
+		}
+		m = floor((left + right) / 2);
+		if (array[m] < value)
+			left = m + 1;
+		else if (array[m] > value)
+			right = m - 1;
+		else
+			return (m);
+	}
+return (-1);
 }
